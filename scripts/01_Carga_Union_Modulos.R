c415c535cd843c64706411e8a9fcfd656ffc89f5
Hasbrali <esteba.raphael@pucp.edu.pe>
2026-06-19 21:49:57 -0500

    Creamos scrip para carga y unión de módulos  de ENAHO

---
+++ scripts/01_Carga_Union_Modulos.R
@@ -0,0 +1,12 @@
+#=====================================================================================
+#Proyecto: PC3-INGRESOS POR IDIOMA
+#Autor: Raphael Esteba
+#Fecha: 19-06-2026
+#Script: Cargar los módulos-realización de joins
+#=====================
+
+#1.Carga de Librerias--------------------------------------------------------
+library(rio)
+library(tidyverse)
+library(janitor)
+library(readr)
