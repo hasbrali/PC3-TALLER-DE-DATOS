f5200fc77ebf1d54679a7e910208762fd9c79dee
Hasbrali <esteba.raphael@pucp.edu.pe>
2026-06-19 22:32:50 -0500

    Se importaron los datos de manera adecuada se probo usar enconding para la ene mayuscula, no funciono. siguiente commit se dara solucion

--- scripts/01_Carga_Union_Modulos.R
+++ scripts/01_Carga_Union_Modulos.R
@@ -11,3 +11,9 @@ library(tidyverse)
 library(janitor)
 library(readr)
 renv::snapshot()
+
+
+#2 Importacion de datos------------------------------------------------------
+mod500 <- import("Datos/Crudos/enaho01a-2024-500.dta")
+mod400 <- import("Datos/Crudos/enaho01a-2024-400.dta")
+sumaria <- import("Datos/Crudos/sumaria-2024-12g.dta")
