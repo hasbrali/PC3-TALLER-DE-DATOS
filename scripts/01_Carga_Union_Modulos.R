76e18fdb20299f4cd73ae80094a5cfb8bc8f60d4
Hasbrali <esteba.raphael@pucp.edu.pe>
2026-06-19 23:54:31 -0500

    Se procedio con la Union de bases y filtrado
    mod500 es empleo
    mod400 salud
    sumaria ingresos
    
    filtramos pues solo necesitamos los jefes de familia que son los de la sumaria

--- scripts/01_Carga_Union_Modulos.R
+++ scripts/01_Carga_Union_Modulos.R
@@ -22,3 +22,19 @@ mod400  <- import("Datos/Crudos/enaho01a-2024-400.dta") %>%
 
 sumaria <- import("Datos/Crudos/sumaria-2024-12g.dta") %>% 
   mutate(across(where(is.character), str_to_lower))
+#3. Union de bases y filtrado---------------------------------------------------------
+
+keys_hogar    <- c("aÑo", "mes", "conglome", "vivienda", "hogar", "ubigeo", "dominio", "estrato")
+keys_personas <- c(keys_hogar, "codperso")
+
+enaho_jefes_2024 <- mod500 %>% 
+  filter(p203 == 1) %>% 
+  left_join(mod400, by = keys_personas) %>% 
+  left_join(sumaria, by = keys_hogar)
+#Verificacion de filas
+nrow(enaho_jefes_2024) == nrow(sumaria)
+
+gc()
+
+
+
