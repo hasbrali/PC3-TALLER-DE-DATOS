69008493bc2667a9f2010aebe665a797d93f9a02
Hasbrali <esteba.raphael@pucp.edu.pe>
2026-06-19 19:45:28 -0500

    Initial commit

---
+++ PC3_Enlace.R
@@ -0,0 +1,16 @@
+#=====================================================
+#Proyecto: PC3-INGRESOS POR IDIOMA
+#Autor: Raphael Esteba
+#Fecha: 19-06-2026
+#====================================================
+
+#Creación de Carpetas------------------
+
+dir.create("Datos")
+dir.create("Datos/Crudos")
+dir.create("Datos/procesados")
+dir.create("outputs")
+dir.create("docs")
+
+
+#Enlace con Git y Github
