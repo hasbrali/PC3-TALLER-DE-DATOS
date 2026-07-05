a96571a17923b4b4cb00813054ea538b045232ce
Hasbrali <esteba.raphael@pucp.edu.pe>
2026-07-05 18:27:47 -0500

    Creación del script de acondicionamiento de datos
    Carga de librerias
    janitor limpieza de datos
    naniar grafico y summary de NAs
    
    snapshot de las versiones de las librerias

---
+++ PC4_Acondicionamiento de Datos.R
@@ -0,0 +1,14 @@
+#=====================================================================================
+#Proyecto: PC4-INGRESOS POR IDIOMA
+#Autor: Raphael Esteba
+#Fecha: 05-07-2026
+#Script: ACONDICIONAMIENTO DE DATOS (ENFOQUE: JEFES DE FAMILIA)
+#=====================
+
+#1.Carga de Librerias--------------------------------------------------------
+library(tidyverse)
+library(arrow)
+library(janitor)
+library(naniar)
+
+renv::snapshot()
