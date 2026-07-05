9e93e63a02f2747590eef9b540bbfbe8675e63c7
Hasbrali <esteba.raphael@pucp.edu.pe>
2026-07-05 18:38:09 -0500

    Carga de Base unida
    se usa el parquet exportado en la anterior entrega del script de Carga_Union_Modulos

--- PC4_Acondicionamiento de Datos.R
+++ PC4_Acondicionamiento de Datos.R
@@ -5,10 +5,17 @@
 #Script: ACONDICIONAMIENTO DE DATOS (ENFOQUE: JEFES DE FAMILIA)
 #=====================
 
-#1.Carga de Librerias--------------------------------------------------------
+#1.Carga de Librerias-----------------------------------------------------------
 library(tidyverse)
 library(arrow)
 library(janitor)
 library(naniar)
 
 renv::snapshot()
+
+#2.Carga, selección y renombrado de variables de interés------------------------
+
+# Cargamos la base integrada
+base_unida <- read_parquet("Datos/procesados/enaho_jefes_2024_190626.parquet")
+
+
