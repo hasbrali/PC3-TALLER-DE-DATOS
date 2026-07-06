a111bac1f76236a1c451c5ff064a17d06b5e3d02
Hasbrali <esteba.raphael@pucp.edu.pe>
2026-07-05 21:03:21 -0500

    Creación del Script de Exploración Se cargaron las librerias tidyverse, arrow, scales
    Se cargo el parquet de enaho_jefes_acondicionada
    
    La recodificación responde a un proceso de estandarización.
    Tambien transformamos los codigos numericos a categorias.
    En el caso del idioma, agrupar las lenguas originarias frente al castellano permite capturar la brecha de adscripción lingüística y étnica que estructura la desigualdad histórica en el país. Por su parte, colapsar el nivel educativo en tres grandes tramos (primaria o menos, secundaria, y superior) simplifica la visualización de los datos y facilita la identificación de los niveles.

---
+++ scripts/03_Clasificación.R
@@ -0,0 +1,48 @@
+#=====================================================================================
+#Proyecto: PC4-INGRESOS POR IDIOMA
+#Autor: Raphael Esteba
+#Fecha: 05-07-2026
+#Script: EXPLORACIÓN DE DATOS (ENFOQUE: JEFES DE FAMILIA)
+#=====================
+#1.Carga de Librerias y Base de Datos-----------------------------------------------------------
+library(tidyverse)
+library(arrow)
+library(scales)
+#Base acondicionada
+jefes_acondicionada <- read_parquet("Datos/procesados/enaho_jefes_acondicionada.parquet")
+
+#2. Recodificación Metodológica
+
+#Creación de códigos numéricos a etiquetas 
+
+jefes_explora <- jefes_acondicionada %>%
+  mutate(
+    # Lengua Materna (p300a)
+    idioma_factor = case_when(
+      lengua_materna == 1 ~ "Quechua",
+      lengua_materna == 2 ~ "Aimara",
+      lengua_materna %in% c(3, 4) ~ "Otras Lenguas Nativas",
+      lengua_materna == 5 ~ "Castellano",
+      TRUE ~ "Otros/No especificado"
+    ),
+    idioma_factor = factor(idioma_factor, levels = c("Castellano", "Quechua", "Aimara", "Otras Lenguas Nativas")),
+    
+    # Nivel Educativo (p301a)
+    educ_factor = case_when(
+      nivel_edu %in% c(1, 2, 3) ~ "Sin Educ / Primaria",
+      nivel_edu %in% c(4, 5)    ~ "Secundaria",
+      nivel_edu %in% c(6, 7, 8) ~ "Superior (Tec/Univ)",
+      TRUE ~ "No especificado"
+    ),
+    educ_factor = factor(educ_factor, levels = c("Sin Educ / Primaria", "Secundaria", "Superior (Tec/Univ)")),
+    
+    # Sexo (p207)
+    sexo_factor = case_when(
+      sexo == 1 ~ "Hombre",
+      sexo == 2 ~ "Mujer",
+      TRUE ~ NA_character_
+    )
+  )
+
+
+
