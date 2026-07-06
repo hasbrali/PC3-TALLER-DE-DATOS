3c410b890fe21bf004182c29088e223d686e3728
Hasbrali <esteba.raphael@pucp.edu.pe>
2026-07-05 20:44:38 -0500

    Hemos filtramos para conservar solo jefes de hogar (código 1), mayores de 14 años y con ingresos mayores a cero.
    Se exporto la tabla parquet en la carpeta de Datos/procesados

--- PC4_Acondicionamiento de Datos.R
+++ PC4_Acondicionamiento de Datos.R
@@ -42,3 +42,26 @@ base_acondicionada <- base_unida %>%
     sector_empleo = p510           # Sector de empleo institucional
   )
 
+
+#3. Diagnóstico y Reporte de Datos Perdidos (NAs)-------------------------------
+
+#Reporte Gráfico de NAs
+grafico_nas_jefes <- gg_miss_var(base_acondicionada, show_pct = TRUE) +
+  labs(
+    title = "Porcentaje de Valores Perdidos (NAs) por Variable",
+    subtitle = "PC4: Diagnóstico analítico con variables renombradas (2024)",
+    y = "% de Datos Perdidos",
+    x = "Variables del Proyecto"
+  ) +
+  theme_minimal()
+
+# Guardamos el gráfico
+ggsave("outputs/Grafico_NAs_Jefes.png", plot = grafico_nas_jefes, 
+       width = 8, height = 6, bg = "white")
+
+#4. Filtrado Muestral Estricto y Exportación Final------------------------------
+#Conservamos solo jefes de hogar (código 1), mayores de 14 años y con ingresos mayores a cero.
+jefes_filtrada <- base_acondicionada %>%
+  filter(jefe_familia == 1, !is.na(ingreso_bruto), ingreso_bruto > 0, edad >= 14)
+
+write_parquet(jefes_filtrada, "Datos/procesados/enaho_jefes_acondicionada.parquet")
